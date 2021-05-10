32ac7b7f9e512e54cf3dc4020ba1c9951a327930
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2021-05-10 12:57:14 -0400

    Postorder iterator algorithm

--- src/bst/iter.rs
+++ src/bst/iter.rs
@@ -84,4 +84,47 @@ impl<'a, K: Ord, V: PartialEq> Iterator for NodeIterInorder<'a, K, V> {
 
 pub struct NodeIterPostorder<'a, K: Ord, V: PartialEq> {
   stack: Vec<&'a Node<K, V>>,
+  current: Option<&'a Node<K, V>>,
+}
+
+impl<'a, K: Ord, V: PartialEq> NodeIterPostorder<'a, K, V> {
+  pub fn new(root: Option<&'a Node<K, V>>) -> Self {
+    Self {
+      stack: Vec::new(),
+      current: root,
+    }
+  }
+}
+
+impl<'a, K: Ord, V: PartialEq> Iterator for NodeIterPostorder<'a, K, V> {
+  type Item = &'a Node<K, V>;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    loop {
+      let mut cur = self.current;
+      while let Some(node) = cur {
+        if let Some(right) = node.right.as_deref() {
+          self.stack.push(right);
+        }
+        self.stack.push(node);
+        cur = node.left.as_deref();
+      }
+      cur = self.stack.pop();
+      match cur {
+        None => return None,
+        Some(node) => {
+          if let (Some(right), Some(top)) = (node.right.as_deref(), self.stack.last()) {
+            // Top of the stack is the right child of the current popped node
+            if right.key == top.key {
+              self.current = self.stack.pop();
+              self.stack.push(node);
+              continue;
+            }
+          }
+          self.current = None;
+          return Some(node);
+        }
+      }
+    }
+  }
 }
